41b777bb2102f4f766ac1e15a6fe0f23858ecc35
Andrew Affolter <andrewaffolter@gmail.com>
2015-03-02 15:12:38 -0800

    Commiting the guided tour playground from Apple to work through it

---
+++ GuidedTour.playground/section-11.swift
@@ -0,0 +1,10 @@
+let apples = 3
+let oranges = 5
+let appleSummary = "I have \(apples) apples."
+let fruitSummary = "I have \(apples + oranges) pieces of fruit."
+
+let height:Double = 68
+let name = "Andrew"
+let myHeight = "\(name)'s height is \(height + 0.5) inches"
+
+
---
+++ GuidedTour.playground/section-9.swift
@@ -0,0 +1,4 @@
+let label = "The width is "
+let width = 94
+let widthLabel = label + String(width)
+//swift thinks that width is a Int and you cannot implicitly convert because Swift sees lable as a String
